899d57b14318a0bba0829fc5cb67e999eb7d6575
p1nheiros <pinheiros.dev@gmail.com>
2025-09-15 23:27:23 -0300

    feat: py music bot code is ready in my prod env

---
+++ code/main.py
@@ -0,0 +1,115 @@
+import discord
+from discord.ext import commands
+import yt_dlp
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# config do token, das intenções do bot e do prefixo de comando
+TOKEN = os.getenv("TOKEN") 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+botCmd = commands.Bot(command_prefix="!", intents=intents)
+
+# guarda as filas de cada server em um dicionário, permitindo funcionar em vários servidores sem misturar filas
+filas = {}
+
+# func p gerenciar fila
+async def proxima_msc(bot):
+    # func chamada p tocar próxima msc
+    if bot.guild.id in filas and filas[bot.guild.id]:
+        # busca primeira msc da fila
+        proxMsc = filas[bot.guild.id].pop(0)
+        urlMsc = proxMsc['url']
+        msc = proxMsc['titulo']
+
+        FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        src = await discord.FFmpegOpusAudio.from_probe(urlMsc, **FFMPEG_OPTS)
+        
+        # cria um loop, tocando a próxima msc e logo depois programa para a função ser chamada novamente quando a msc atual terminar
+        bot.voice_client.play(src, after=lambda _: botCmd.loop.create_task(proxima_msc(bot)))
+        
+        await bot.send(f"▶️ Tocando a seguir: **{msc}**")
+
+# evento e comando do bot
+@botCmd.event
+async def on_ready():
+    print(f'{botCmd.user.name} está online!')
+
+@botCmd.command(name='pinhas')
+async def tocar(bot, *, query: str):
+    if bot.author.voice is None:
+        await bot.send("Você precisa estar em uma call!")
+        return
+    
+    call = bot.author.voice.channel
+    if bot.voice_client is None:
+        await call.connect()
+    else:
+        await bot.voice_client.move_to(call)
+
+    await bot.send(f"🔍 DJ Pinhas está procurando por **`{query}`**...")
+
+    YDL_OPTS = {'format': 'bestaudio/best', 'noplaylist': 'True', 'default_search': 'auto'}
+    try:
+        with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
+            info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+        
+        urlMsc = info['url']
+        nomeMsc = info['title']
+        msc = {'url': urlMsc, 'titulo': nomeMsc}
+
+        if bot.guild.id not in filas:
+            filas[bot.guild.id] = []
+        
+        if bot.voice_client.is_playing() or bot.voice_client.is_paused():
+            filas[bot.guild.id].append(msc)
+            await bot.send(f"✅ DJ Pinhas adicionou **{nomeMsc}** à fila")
+        else:
+            FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            src = await discord.FFmpegOpusAudio.from_probe(urlMsc, **FFMPEG_OPTS)
+            
+            # quando termina de tocar a msc(src) executa a func proxima_msc
+            bot.voice_client.play(src, after=lambda _: botCmd.loop.create_task(proxima_msc(bot)))
+            
+            await bot.send(f"▶️ DJ Pinhas está tocando agora: **{nomeMsc}**")
+
+    except Exception as e:
+        await bot.send("Perdoa o Pinhas, não consegui encontrar a música😔😔😭😭😭")
+        print(f"Erro no comando !pinhas: {e}")
+
+@botCmd.command(name='prox')
+async def pular(bot):
+    if bot.voice_client and bot.voice_client.is_playing():
+        bot.voice_client.stop() # quando a msc atual é parada aciona a func after, chamando a func proxima_msc, pulando a msc
+        await bot.send("DJ Pinhas pulou a música! ⏭️")
+    else:
+        await bot.send("DJ Pinhas não está tocando nenhuma música no momento.")
+
+@botCmd.command(name='fila')
+async def fila(bot):
+    if bot.guild.id in filas and filas[bot.guild.id]:
+        lista = "```🎵 Fila de Músicas do DJ Pinhas 🎵\n\n"
+        for i, msc in enumerate(filas[bot.guild.id]):
+            lista += f"{i + 1}. {msc['titulo']}\n"
+        lista += "```"
+        await bot.send(lista)
+    else:
+        await bot.send("A fila está vazia! Use `!pinhas` para adicionar músicas.")
+        
+@botCmd.command(name='desc')
+async def sair(bot):
+    # limpa a fila do server antes de sair
+    if bot.guild.id in filas:
+        filas[bot.guild.id] = []
+
+    if bot.voice_client:
+        await bot.voice_client.disconnect()
+        await bot.send("Até mais! DJ Pinhas limpou a fila de músicas.")
+    else:
+        await bot.send("DJ Pìnhas não está em um canal de voz.")
+
+botCmd.run(TOKEN)
